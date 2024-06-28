efe0f839e5be3e510ea0014459843a91ba303492
MaidaRehmanKhan <160591546+MaidaRehmanKhan@users.noreply.github.com>
2024-06-28 04:15:29 -0700

    Add files via upload

---
+++ q15.js
@@ -0,0 +1,14 @@
+"use strict";
+// changing Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+//• Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+//• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+//• Print a second set of invitation messages, one for each person who is still in your list.
+// let  guestArr : string[] = ["Amna", "Faryal", "Faisal", "Shafak"]
+// let canNotAttend : string = "Faryal"
+// console.log(canNotAttend + " " + "can not make it, for dinner")
+// let newGuest : string = "Sufiyan"
+// guestArr[guestArr.indexOf(canNotAttend)] = newGuest;
+// console.log(guestArr)
+// guestArr.map((item) => 
+//         console.log(`Dear ${item}, Are You Cordinally Invited To A Dinner`)
+// );
