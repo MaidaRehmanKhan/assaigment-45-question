84ea476ddb9810875daff06229e20ce5dfe4d567
MaidaRehmanKhan <160591546+MaidaRehmanKhan@users.noreply.github.com>
2024-06-28 06:07:53 -0700

    Add files via upload

---
+++ q41.js
@@ -0,0 +1,9 @@
+"use strict";
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+//  function show_magicians(Magicians:string[]):void{
+//     for(const magician of Magicians){
+//         console.log(magician);
+//     }
+//  }
+//  const Magicians: string[] = ["penn jillette", "criss angel", "teller"];
+//  show_magicians(Magicians)
