be7b3e0c07d8f1c12110af56721db05e6f281f64
MaidaRehmanKhan <160591546+MaidaRehmanKhan@users.noreply.github.com>
2024-06-28 04:23:26 -0700

    Add files via upload

---
+++ q23.ts
@@ -0,0 +1,31 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+// let car = "subaru";
+// //console.log("Is car =='subaru' ? I pridict otherwise I true otherise I pridict false")
+// console.log(car == 'subaru');
+// console.log(car == 'civic');
+
+// let num : any = 23;
+// //console.log("Is num === 23(in number) ? I predict true otherwise predict false")
+// console.log(num === 23);
+// console.log(num === "23");
+
+// let flower = "red";
+// //console.log("Is flower ==red ? I predict true otherwise I predict false")
+// console.log(flower == "red");
+// console.log(flower == "pink");
+
+// let day = "friday";
+// //console.log("Is day ==friday ? I predict true otherwise I predict false")
+// console.log(day == "friday");
+// console.log(day == "saterday");
+
+// let name = "hira";
+// //console.log("Is name == hira ? I predict true otherwise I predict false")
+// console.log(name == "hira");
+// console.log(name == "sana");
