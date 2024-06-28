31c03a78918aed4d78995f8625cd6ff2c900e600
MaidaRehmanKhan <160591546+MaidaRehmanKhan@users.noreply.github.com>
2024-06-28 06:08:30 -0700

    Add files via upload

---
+++ q42.js
@@ -0,0 +1,9 @@
+"use strict";
+//Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+// function show_magicians(Magicians:string[]):void{
+//     for(const magician of Magicians){
+//         console.log(magician + " " + "The Great");
+//     }
+//  }
+//  const Magicians: string[] = ["penn jillette", "criss angel", "teller"];
+//  show_magicians(Magicians)
