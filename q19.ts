5c75b74368b2ba28090ab51691cbe1f959aa631b
MaidaRehmanKhan <160591546+MaidaRehmanKhan@users.noreply.github.com>
2024-06-28 04:19:00 -0700

    Add files via upload

---
+++ q19.ts
@@ -0,0 +1,8 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+
+// let GuestArr : string[] = ["Sidra", "Sikander", "Faryal", "Noman", "Nida"] 
+
+// GuestArr.map((item) => (
+//            console.log(`Dear ${item}, You Are Inviting To Dinner`)
+// ));
