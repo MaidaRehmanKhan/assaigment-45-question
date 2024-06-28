55fa46cf7b35bb60139d09ef6bb123c3b849695f
MaidaRehmanKhan <160591546+MaidaRehmanKhan@users.noreply.github.com>
2024-06-28 05:57:28 -0700

    Add files via upload

---
+++ q31.js
@@ -0,0 +1,21 @@
+"use strict";
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+// let userNames : string[] = ["Amna", "Afsheen", "Kanwal", "Ali"];
+// if(userNames.length === 0){
+//     console.log("We need to find some users!");
+// }
+// else{
+//     for(let i=0; i<userNames.length; i++);
+// }
+// console.log(userNames)
+//2nd method
+//  let userNames : string[] = []
+// if(userNames.length === 0){
+//     console.log("We need to find some users!");
+// }
+// else{
+//     for(let i=0; i<userNames.length; i++);
+// }
+//console.log(userNames)
