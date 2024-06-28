1b3bb235d7c87fa45117d001156877900894814a
MaidaRehmanKhan <160591546+MaidaRehmanKhan@users.noreply.github.com>
2024-06-28 04:17:51 -0700

    Add files via upload

---
+++ q18.js
@@ -0,0 +1,35 @@
+"use strict";
+// Seeing the World: Think of at least five places in the world you’d like to visit.
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
+// • Print your array in its original order.
+// • Print your array in alphabetical order without modifying the actual list.
+// • Show that your array is still in its original order by printing it.
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+// • Show that your array is still in its original order by printing it again.
+// • Reverse the order of your list. Print the array to show that its order has changed.
+// • Reverse the order of your list again. Print the list to show it’s back to its original order.
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
+//let places : string[] = ["Karachi", "Islamabad", "Lahore", "Tokyo", "Multan"];
+//console.log(places);
+// • Print your array in its original order.
+//console.log("Original Order :", places);
+// • Print your array in alphabetical order without modifying the actual list.
+//let sortedPlaces : string[] = places.slice().sort();
+//console.log("Alphabetic Order :", sortedPlaces );
+// • Show that your array is still in its original order by printing it.
+//console.log("Original Order After Sorting :", places);
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+//let reverseSortedPlaces : string[] = places.slice().sort((a , b) => b.localeCompare(a));
+//console.log("Reverse Alphabetic Order :", reverseSortedPlaces);
+// • Show that your array is still in its original order by printing it again.
+//console.log("Original Order After Reverse sorting :", places);
+// • Reverse the order of your list. Print the array to show that its order has changed.
+//console.log(places.reverse());
+//console.log("Reverse Order :", places);
+// • Reverse the order of your list again. Print the list to show it’s back to its original order.
+//console.log(places.reverse());
+//console.log("Original Order After Reverse Sorting :", places);
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+//console.log(places.sort());
+//console.log("Sorted Alphaetic Order :", places);
