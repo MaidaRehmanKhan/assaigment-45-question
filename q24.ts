2b845257b660ee28aca39cb9f50d82a64c2567b9
MaidaRehmanKhan <160591546+MaidaRehmanKhan@users.noreply.github.com>
2024-06-28 04:24:20 -0700

    Add files via upload

---
+++ q24.ts
@@ -0,0 +1,52 @@
+// More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+// • Tests for equality and inequality with string
+// • Tests using the lower case function
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+// • Tests using "and" and "or" operators
+// • Test whether an item is in a array
+// • Test whether an item is not in a array
+
+// • Tests for equality and inequality with string
+
+// const string1 : string = "Apple";
+// const string2 : string = "orange";
+// console.log(string1 !== string2); 
+// console.log(string1 == string2);
+
+//Tests using the lower case function
+
+// const mixedCaseString : string = "hello world";
+
+// console.log(mixedCaseString.toLowerCase() === "hellow world")
+
+//Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+// const num1 : number = 10;
+// const num2 : number = 5;
+
+// console.log(num1 === num2);
+// console.log(num1 !== num2);
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+
+//Tests using "and" and "or" operators
+
+// const statement1 : boolean = true;
+// const statement2 : boolean = false;
+
+// console.log(statement1 && statement2);
+// console.log(statement1 || statement2);
+
+//Test whether an item is in a array
+
+// let days : string [] = ["Monday", "Tuesday", "Thursday", "Friday"];
+
+// console.log(days.includes("Thursday"));
+
+//Test whether an item is not in a array
+
+// let month : string [] = ["January", "Febrary", "March", "April"];
+
+// console.log(month.includes("May"));
